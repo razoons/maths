9978f4a9109a97643945c7b678b841c33ff7d7cb
antoine.sarazin <antoine.sarazin@instant-system.com>
2025-08-05 13:54:36 +0200

    startSetup

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-main class="pa-4">
+      <v-container>
+        <v-btn @click="fetchMessage">Call API</v-btn>
+        <p v-if="message">{{ message }}</p>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('')
+
+const fetchMessage = async () => {
+  const res = await fetch(import.meta.env.VITE_API_URL + '/api/hello')
+  const data = await res.json()
+  message.value = data.message
+}
+</script>
