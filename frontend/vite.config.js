609286d7f61957acce445ebe4fda7c2cc4616a26
antoine.sarazin <antoine.sarazin@instant-system.com>
2025-08-18 21:10:25 +0200

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,30 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue(), VitePWA({
-    registerType: 'autoUpdate',
-    manifest: {
-      name: 'Maths Game',
-      short_name: 'Maths',
-      start_url: '.',
-      display: 'standalone',
-      background_color: '#0aaee3',
-      theme_color: '#f78d17',
-      icons: [
-        {
-          src: 'icon-192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'icon-512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ],
-      scope: "/",
-    }
-  })],
+  plugins: [vue()],
   base: '/',
   build: {
     outDir: 'dist',
