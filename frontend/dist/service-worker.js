36774a8d8a7a05d09be940dca1a7a5f29ac3aa70
antoine.sarazin <antoine.sarazin@instant-system.com>
2025-08-20 14:19:30 +0200

    commit

--- frontend/dist/service-worker.js
+++ frontend/dist/service-worker.js
@@ -1,3 +1,25 @@
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = "my-cache-v1";
+const ASSETS = [
+  "/",          // ta page index.html
+  "/index.html",
+  "/main.js",
+  "/style.css",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
